e5e6da67730eaa28cd4609e1af01ba3a9b6a7f58
Raúl Hernández Martínez <raulhernandez.rlhnmn@gmail.com>
2023-08-21 12:05:25 +0200

    Primer ejemplo para base de datos

--- tfg-editorial/Backend/tfg_backend/sonbuenos/models.py
+++ tfg-editorial/Backend/tfg_backend/sonbuenos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class SonBuenos(models.Model):
     nombre = models.CharField(max_length=50)
-    genero = models.CharField(max_length=50)
+    correo = models.CharField(max_length=50)
 
 class Artista (models.Model):
     nombre = models.CharField(max_length=50)
@@ -12,18 +12,15 @@ class Artista (models.Model):
 class Genero (models.Model):
     nombre = models.CharField(max_length=50)
 
+class EntidadMusical (models.Model):
+    nombre = models.CharField(max_length=50)
+    genero = models.ManyToManyField(Genero)
+    artistas = models.ManyToManyField(Artista)
+
 class Cancion (models.Model):
     nombre = models.CharField(max_length=50)
     url = models.URLField(null=True)
     genero = models.ManyToManyField(Genero)
     duracion = models.DurationField(null=True)
     fecha = models.DateField(null=True)
-
-class EntidadMusical (models.Model):
-    nombre = models.CharField(max_length=50)
-    genero = models.ManyToManyField(Genero)
-    artistas = models.ManyToManyField(Artista)
-    canciones = models.ManyToManyField(Cancion)
-
-
-    
+    entidad = models.OneToOneField(EntidadMusical, on_delete=models.CASCADE)
--- tfg-editorial/Frontend/src/components/views/Sincronizacion.vue
+++ tfg-editorial/Frontend/src/components/views/Sincronizacion.vue
@@ -1,9 +1,32 @@
 <template>
     <div class="container mx-auto py-8 lg:pt-8">
-        <div class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
+        <div
+            class="block rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
             <h5 class="mb-2 text-xl font-medium leading-tight text-black text-center">
                 Catalogo con filtros y busqueda
             </h5>
+
+            <div data-te-datatable-init>
+                <table striped hover :items="canciones" :fields="fields">
+
+                </table>
+
+            </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            fields: [
+                { key: 'nombre', label: 'Nombre' },
+                { key: 'entidad', label: "Entidad" }
+            ],
+            canciones: []
+        }
+    }
+}
+
+</script>
